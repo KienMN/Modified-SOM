9765bd665112d820e2eb0901958b6233ca10e14c
Kien Mai Ngoc <kienmn97@gmail.com>
2019-01-29 17:17:39 +0700

    Basic classes

---
+++ detection/competitive_learning/combined_network.py
@@ -0,0 +1,13 @@
+import numpy as np
+from .som import SOM
+from .lvq_networks import NeighborhoodLVQ
+
+class SOMLVQ(SOM, NeighborhoodLVQ):
+  def __init__(self):
+    pass
+
+  def fit(self, X, y):
+    pass
+
+  def predict(self, X, y):
+    pass
--- detection/competitive_learning/lvq_networks.py
+++ detection/competitive_learning/lvq_networks.py
@@ -6,8 +6,20 @@ class TheoriticalLVQ(CompetitiveNetwork):
   def __init__(self):
     pass
 
-  def fit(self):
+  def fit(self, X, y, labels_proportion = None):
     pass
 
   def predict(self):
     pass
+
+class NeighborhoodLVQ(CompetitiveNetwork):
+  """Modified version of theoretical LVQ"""
+  def __init__(self):
+    pass
+
+  def fit(self, X, y, labels_init = None):
+    pass
+
+  def predict(self, X):
+    pass
+
