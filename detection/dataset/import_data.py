b397985a7c11c6297df230623f2e73541623df46
Kien Mai Ngoc <kienmn97@gmail.com>
2019-04-25 12:13:31 +0700

    Rename dataset to sample well

--- detection/dataset/import_data.py
+++ detection/dataset/import_data.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 def load_dataset():
-  filepath = os.path.dirname(__file__) + '/data/processed_15_1-SD-1X_LQC.csv'
+  filepath = os.path.dirname(__file__) + '/data/sample-well.csv'
   dataset = pd.read_csv(filepath)
   dataset = dataset[['DEPT', 'DT', 'GR', 'NPHI', 'PHIE', 'RHOB', 'VCL', 'DEPO_FACIES']]
   return dataset
