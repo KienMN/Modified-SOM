7667d85148ca828b88dcdf98928280ef18d329f0
Kien Mai Ngoc <kienmn97@gmail.com>
2019-05-02 17:24:06 +0700

    Unit test cases for SOM-LVQ network

--- tests/competitive_learning/test_combine_som_lvq.py
+++ tests/competitive_learning/test_combine_som_lvq.py
@@ -1,14 +1,13 @@
 # Testing combine som and lvq model
 
-# Adding path to libraries
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+# Importing the context
+import context
 
 # Importing the libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import unittest
 
 # Importing the dataset
 from detection.dataset import load_dataset
@@ -31,48 +30,35 @@ from sklearn.preprocessing import LabelEncoder
 encoder = LabelEncoder()
 y_train = encoder.fit_transform(y_train)
 
-# Training the LVQ
+# Testing the SOM-LVQ network
 from detection.competitive_learning import CombineSomLvq
-model = CombineSomLvq(n_rows = 10, n_cols = 10)
-model.fit(X_train, y_train, weights_init = None, labels_init = None,
-          unsup_num_iters = 0, unsup_batch_size = 10,
-          sup_num_iters = 50, sup_batch_size = 10,
-          neighborhood = "bubble",
-          learning_rate = 0.5, learnining_decay_rate = 1, learning_rate_decay_function = None,
-          sigma = 1, sigma_decay_rate = 1, sigma_decay_function = None,
-          conscience = False, verbose = 0)
-
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.naive_bayes import GaussianNB
-# from sklearn.neighbors import KNeighborsClassifier
-# classifier = KNeighborsClassifier(20)
-# classifier.fit(X_train, y_train)
-
-
-# Predict the result
-y_pred, confidence_score, distances = model.predict(X_test, confidence_score = True, distance_to_bmu = True)
-# y_pred = classifier.predict(X_test)
-for i in range (len(y_pred)):
-  print(y_pred[i] == y_test[i], confidence_score[i], distances[i])
-
-y_pred = encoder.inverse_transform(y_pred)
-
-# Making confusion matrix
 from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
 
-# Printing the confusion matrix
-print(cm)
-true_result = 0
-for i in range (len(cm)):
-  true_result += cm[i][i]
-print(true_result / np.sum(cm))
+class TestCombineSomLvq(unittest.TestCase):
+  def test_training(self):
+    model = CombineSomLvq(n_rows = 10, n_cols = 10)
+    model.fit(X_train, y_train, weights_init = None, labels_init = None,
+              unsup_num_iters = 10, unsup_batch_size = 10,
+              sup_num_iters = 10, sup_batch_size = 10,
+              neighborhood = "bubble",
+              learning_rate = 0.5, learnining_decay_rate = 1, learning_rate_decay_function = None,
+              sigma = 1, sigma_decay_rate = 1, sigma_decay_function = None,
+              conscience = False, verbose = False)
+    self.assertEqual(model._competitive_layer_weights.shape, (10 * 10, X_train.shape[1]))
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set()
-plt.plot(model._quantization_error)
-plt.xlabel("Number of iterations")
-plt.ylabel("Quantization error")
-plt.title("Quantization error through iterations")
-plt.show()
+  def test_predicting(self):
+    model = CombineSomLvq(n_rows = 10, n_cols = 10)
+    model.fit(X_train, y_train, weights_init = None, labels_init = None,
+              unsup_num_iters = 10, unsup_batch_size = 10,
+              sup_num_iters = 10, sup_batch_size = 10,
+              neighborhood = "bubble",
+              learning_rate = 0.5, learnining_decay_rate = 1, learning_rate_decay_function = None,
+              sigma = 1, sigma_decay_rate = 1, sigma_decay_function = None,
+              conscience = False, verbose = False)
+    y_pred = model.predict(X_test, confidence_score = False, distance_to_bmu = False)
+    y_pred = encoder.inverse_transform(y_pred)
+    cm = confusion_matrix(y_test, y_pred)
+    self.assertEqual(y_pred.shape, y_test.shape)
+
+if __name__ == '__main__':
+  unittest.main()
