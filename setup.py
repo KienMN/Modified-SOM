032480c6943e090831066d023af6b09378c812b0
Kien Mai Ngoc <kienmn97@gmail.com>
2019-06-14 11:11:19 +0700

    Init setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup, find_packages
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+readme = open(path + "/docs/README.md")
+
+setup(
+  name="modified-som",
+  version="1.0.0",
+  description="A modified version of SOM and LVQ for classification problems",
+  url="https://github.com/KienMN/Modified-SOM",
+  author="Kien MN",
+  author_email="kienmn97@gmail.com",
+  license="MIT",
+  packages=find_packages(exclude=["docs","tests", ".gitignore"])
+)
